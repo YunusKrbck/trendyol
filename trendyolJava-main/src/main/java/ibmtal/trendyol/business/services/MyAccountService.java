d316967c789c1953b80b95dcc971136b9b10a5e2
YunusKrbck <102135684+YunusKrbck@users.noreply.github.com>
2022-03-22 15:36:06 +0300

    Add files via upload

--- trendyolJava-main/src/main/java/ibmtal/trendyol/business/services/MyAccountService.java
+++ trendyolJava-main/src/main/java/ibmtal/trendyol/business/services/MyAccountService.java
@@ -1,5 +1,21 @@
 package ibmtal.trendyol.business.services;
 
-public interface MyAccountService {
+import java.util.ArrayList;
 
+import com.sun.net.httpserver.Authenticator.Result;
+
+import ibmtal.trendyol.dto.MyAccountDto;
+import ibmtal.trendyol.entity.MyAccount;
+
+
+
+
+
+public interface MyAccountService {
+	ArrayList<MyAccount> getAll();
+	Result addMyAccount(MyAccount myaccount);
+	Result addMyAccountDto(MyAccountDto myaccountDto);
+	Result addCompany(MyAccount myaccount);
+	Result getCompany(Integer id);
+	Result getMyAccount(Integer id);
 }
